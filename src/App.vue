2b740f840743ad70e2f1454b9c006200865341dd
Joe <joesmith0488@gmail.com>
2017-01-26 18:49:52 +0000

    Initial commit with pug and stylus setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+#app
+  img(src="./assets/logo.png")
+  hello
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
